b4973e6d4ab180741c90207384faaf7288a4ae9a
Fauzan <fauzan.c4@gmail.com>
2025-01-19 09:10:26 +0700

    add sample insert data db

---
+++ db/init.sql
@@ -0,0 +1,8 @@
+INSERT INTO file_explorer_db.Folder
+VALUES
+(3, 'root', NULL, '2025-01-17 19:06:29.994', NULL),
+(4, 'directory 4', 1, '2025-01-18 23:28:57.046', '2025-01-18 23:28:57.046');
+
+INSERT INTO file_explorer_db.File
+(id, name, folder_id, created_at, updated_at)
+VALUES(1, 'file_pertama.csv', 27, '2025-01-18 23:39:14.540', NULL);
